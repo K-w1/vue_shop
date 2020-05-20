a4d333c4db88cdc37ac23d271806c9207773c259
634717710@qq.com <634717710@qq.com>
2020-05-20 13:51:30 +0800

    完结

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -14,11 +14,77 @@
           </el-input>
         </el-col>
       </el-row>
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款">
+          <template slot-scope="scope">
+              <el-tag type="primary" v-if="scope.row.pay_status==1">已付款</el-tag>
+              <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间">
+          <template slot-scope="scope">
+            {{scope.row.create_time|timeFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
+            <el-button size="mini" type="success" icon="el-icon-location" @click="showProgressBox"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+      <el-dialog
+        title="修改地址"
+        :visible.sync="addAddressVisible"
+        @close="addressDialogClosed"
+        width="50%">
+        <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
+          <el-form-item label="省市区/县" prop="address1">
+            <el-cascader :options="cityData" v-model="addressForm.address1"></el-cascader>
+          </el-form-item>
+          <el-form-item label="详细地址" prop="address2">
+            <el-input v-model="addressForm.address2"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="addAddressVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addAddressVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+
+      <el-dialog
+        title="物流进度"
+        :visible.sync="progressVisible"
+        @close="progressDialogClosed"
+        width="50%">
+        <el-timeline :reverse="reverse">
+          <el-timeline-item
+            v-for="(activity, index) in progressInfo"
+            :key="index"
+            :timestamp="activity.time">
+            {{activity.context}}
+          </el-timeline-item>
+        </el-timeline>
+      </el-dialog>
     </el-card>
   </div>
 </template>
 
 <script>
+import cityData from './citydata'
   export default {
     data(){
       return{
@@ -28,7 +94,23 @@
           pagesize:10
         },
         orderList:[],
-        total:0
+        total:0,
+        addAddressVisible:false,
+        addressForm:{
+          address1:[],
+          address2:''
+        },
+        addressFormRules:{
+          address1:[
+            {required:'true',message:'请选择省市区县',trigger:'blur'}
+          ],
+          address2:[
+            {required:'true',message:'请输入具体地址',trigger:'blur'}
+          ],
+        },
+        cityData,
+        progressVisible:false,
+        progressInfo:[]
       }
     },
     created(){
@@ -44,11 +126,36 @@
         console.log(res.data)
         this.orderList=res.data.goods
         this.total=res.data.total
+      },
+      handleSizeChange(newSize){
+        this.queryInfo.pagesize=newSize
+        this.getOrderList()
+      },
+      handleCurrentChange(newPage){
+        this.queryInfo.pagenum=newPage
+        this.getOrderList()
+      },
+      showBox(){
+        this.addAddressVisible=true
+      },
+      addressDialogClosed(){
+        this.$refs.addressFormRef.resetFields()
+      },
+      async showProgressBox(){
+        const {data:res} = await this.$http.get('/kuaidi/804909574412544580')
+        if(res.meta.status!=200) return this.$message.error('获取物流信息失败')
+        this.progressInfo=res.data
+        this.progressVisible=true
+      },
+      progressDialogClosed(){
+
       }
     }
   }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.el-cascader{
+  width: 100%;
+}
 </style>
---
+++ src/main-prod.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+// import './assets/css/global.css'
+// import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+import VueQuillEditor from 'vue-quill-editor'
+// import 'quill/dist/quill.core.css' // import styles
+// import 'quill/dist/quill.snow.css' // for snow theme
+// import 'quill/dist/quill.bubble.css' // for bubble theme
+
+import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+
+import axios from 'axios'
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  NProgress.start()
+  config.headers.Authorization=sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http=axios
+
+Vue.use(VueQuillEditor, /* { default global options } */)
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
+
+Vue.filter('timeFormat',function(val){
+  let dt = new Date(val)
+  let y = dt.getFullYear()
+  let m = (dt.getMonth()+1+'').padStart(2,'0')
+  let d = (dt.getDate()+'').padStart(2,'0')
+  let hh=(dt.getHours()+'').padStart(2,'0')
+  let mm=(dt.getMinutes()+'').padStart(2,'0')
+  let ss=(dt.getSeconds()+'').padStart(2,'0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
